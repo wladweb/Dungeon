9c6a790a52b4b1ff6ac6a3b68a46f8efe7b3628c
wladweb <wladwebwork@gmail.com>
2020-09-10 23:09:31 +0300

    added studio mover

---
+++ Assets/Scripts/StudioMover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class StudioMover : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _sensetive;
+
+    private void Update()
+    {
+        Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, _player.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _sensetive);
+    }
+}
